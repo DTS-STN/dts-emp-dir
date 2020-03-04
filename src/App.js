17ca64e6a036d616eac3665bb9650f67fcf33d5c
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2020-03-04 13:44:20 -0500

    saving change to app.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
   return (
     <div className="App">
 
-
-<Header /> 
+      <Header /> 
       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
