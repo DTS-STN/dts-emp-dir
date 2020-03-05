7f31a3c2aeced5f7572473bd087c2f455e491ea9
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-03-05 18:31:20 +0000

    attempting to fix test

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,8 +1,6 @@
 
 /* eslint-disable no-undef */
 
-import { headerImg, langLink, privacyLink, tocLink, aboutCA, sMedia, mobileApp, aboutCAHref, sMediaHref, mobileHref, tocHref, privacyHref, footerImg } from './utils'
-
 function checkA11y(cy) {
     cy.checkA11y({
         runonly: {
