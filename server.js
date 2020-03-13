2e1c9f59c2c29958f37c371830aed7bf338a6a85
Michael Pond <michael.pond@hrsdc-rhdcc.gc.ca>
2020-03-13 19:38:05 +0000

    adding server code - almost ready for datbase connectivity

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+
 const express = require('express');
+const mongoose = require('mongoose');
+// const cors = require('cors');
+const router = require('./routes/index');
+
 const app = express();
-const PORT = 3001;
+const PORT = 6001;
+const MONGO_CONN_STR = ``;
+
+//app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use('/api', router);
+
+mongoose.connect(MONGO_CONN_STR, { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connection.once('open', function(){
+    console.log('connected to the Database.');
+});
+mongoose.connection.on('error', function(error) {
+    console.log('Mongoose Connection Error : ' + error);
+});
 
 console.log(process.env.MONGO_USER);
 
