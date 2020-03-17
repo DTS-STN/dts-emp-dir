4162600f1cb1cf0071842bf5d3db13aeb52f28a8
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2020-03-17 16:40:03 -0400

    saving after changing folder name

--- _client/src/components/SearchBox.js
+++ _client/src/components/SearchBox.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
+//import { connect } from 'react-redux';
 import { getEmployees } from '../actions/getEmployees';
 
-function SearchBox {
+function SearchBox () {
 
   function handleSubmit(event) {
     event.preventDefault();
     getEmployees (event.text)
   }
 
-  render() {
-    return (
-      <div>
-      <h1>Edit {article.title}</h1>
-      <hr/>
+  
+  return (
+    <div>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Search by : </label>
-          <input type="text" name="title" value={article.title} onChange={handleChange} className="form-control" /> 
+          <input type="text" name="title" value="" placeholder="Enter search criteria" onChange={handleSubmit} className="form-control" /> 
         </div>
         <div className="btn-group">
           <button type="submit" className="btn btn-primary">Update</button>
         </div>
       </form>
     </div>
-    )
-  }
+  )
 }
 
-const mapStateToProps = state => ({
-  employees: state.employees,
-});
+// const mapStateToProps = state => ({
+//   employees: state.employees,
+// });
 
-export default connect( mapStateToProps, { getEmployees } )(SearchBox);
+//export default connect( mapStateToProps, { getEmployees } )(SearchBox);
+export default SearchBox;
