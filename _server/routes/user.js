ad035809914d6df10476fab6012acd0fec45f27b
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2020-03-22 16:08:36 -0400

    added auth users and code for authorization

---
+++ _server/routes/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    default: function() {if (this.role !== 'admin') {return  'read-only'}}
+  },
+  locationId: {
+    type: Number,
+    required: true
+  },
+  register_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
